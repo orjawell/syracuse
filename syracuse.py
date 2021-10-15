a582d95f9642f1d662fa2aa1e2699fbae468b301
Akuramu <69430732+orjawell@users.noreply.github.com>
2021-10-15 22:05:44 +0200

    Update syracuse.py
    Ajout d'un timer + commentaires + description + graph + non optimisation du code par pure flemme.

--- syracuse.py
+++ syracuse.py
@@ -1,37 +1,73 @@
 
+reseach_mode = False 
+# Mode de recherche, afin de voir toutes les itérations en cours. Cela peut prendre plus de temps à faire la recherche, je vous déconseille de passer cela sur vrai. (Sauf si vous en avez besoin)
+
+import time as t
 import matplotlib.pyplot as plt
 
+
+def convert(sec):
+    mins = sec // 60
+    sec = round(sec%60,0)
+    hours = mins //60
+    mins = mins  %60
+    return int(hours),int(mins),sec
+
+
 def syracuse(x,iteration):
-    iteration_liste = []
-    result = []
+   
+    """
+    Parameters
+    ----------
+    x : INT
+        X de base.
+    iteration : INT
+        Nombres de vol au quel on souhaite dépasser.
+
+    Returns
+    -------
+    val : INT
+        x au quel le nombre de vol > vol recherché.
+    i : INT
+        Nombre de vol trouvé..
+
+    """
+    if x == 0 or x == 1:
+        return
     i = 0
     val = x
+    start = t.time()
     while i <= iteration:
         i = 0
+        iteration_liste = []
+        result = []
         if x != val:
             x = val + 1
             val = val + 1
-            #print(val)
+            if reseach_mode != False:
+                print('new x: ', val)
         while x!=1:
-            #iteration = []
-            #result = []
             if x%2==0:
                 x = x/2
                 i = i+1
-                #iteration.append(i)
-                #result.append(x)
-                #print("new val: ",x)
+                iteration_liste.append(i)
+                result.append(x)
+                if reseach_mode != False:
+                    print("intermediate result: ",x)
             else:
                 x = (3*x)+1
                 i = i+1
-                #iteration.append(i)
-                #result.append(x)
-                #print("new val: ",x)
-
-    print(val)
-    print(i)
-    #plt.plot(iteration, result)
-    #plt.ylabel("résultat")
-    #plt.xlabel("itération")
-    #plt.show()
-
+                iteration_liste.append(i)
+                result.append(x)
+                if reseach_mode != False:
+                    print("intermediate result: ",x)
+    stop = t.time()
+    temps_passe_s = stop - start
+    temps_final = convert(temps_passe_s)
+    plt.plot(iteration_liste, result, label="Résultat/Nb d'itération")
+    plt.ylabel("résultat")
+    plt.xlabel("itération")
+    plt.legend()
+    plt.show()
+    print('time_elapsed : ', temps_final)
+    return val,i
